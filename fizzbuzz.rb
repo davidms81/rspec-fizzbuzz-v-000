480668b8bbf38144022f817b318f0bda3d91d1df
David Schoenholtz <dmschoenholtz@gmail.com>
2020-03-25 15:04:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,6 @@
 def fizzbuzz(number)
   if number % 3 == 0
     puts "Fizz"
-  elsif number % 5 == 0
-    puts "Buzz"
-  elsif number % 3 == 0 && number % 5 == 0
-    puts "FizzBuzz"
+  
   end
 end
